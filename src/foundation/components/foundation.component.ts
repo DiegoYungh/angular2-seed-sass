3c45bb57af2c51b8fb80f34e7f1a3479efdb2a03
Diego Yungh <diego.yungh@gmail.com>
2016-04-18 11:53:07 -0300

    add: modifications to use foundation sites

---
+++ src/foundation/components/foundation.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'sd-foundation',
+  moduleId: module.id,
+  templateUrl: './foundation.component.html',
+  styleUrls: ['./foundation.component.css']
+})
+export class FoundationComponent {}
--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -1,6 +1,6 @@
 import {join} from 'path';
 import {SeedConfig} from './seed.config';
-import {InjectableDependency} from './seed.config.interfaces';
+import {InjectableDependency, SassOptions} from './seed.config.interfaces';
 
 export class ProjectConfig extends SeedConfig {
   PROJECT_TASKS_DIR = join(process.cwd(), this.TOOLS_DIR, 'tasks', 'project');
@@ -9,9 +9,18 @@ export class ProjectConfig extends SeedConfig {
     super();
     // this.APP_TITLE = 'Put name of your app here';
     let additional_deps: InjectableDependency[] = [
-      // {src: 'jquery/dist/jquery.min.js', inject: 'libs'},
+      {src: 'jquery/dist/jquery.min.js', inject: 'libs'},
+      {src: 'foundation-sites/dist/foundation.js', inject: 'libs'},
       // {src: 'lodash/lodash.min.js', inject: 'libs'},
     ];
+
+    let custom_sass_options: SassOptions = {
+      includePaths: [
+        'node_modules/foundation-sites/scss'
+      ]
+    };
+
+    this.SASS_OPTIONS = Object.assign(this.SASS_OPTIONS, custom_sass_options);
 
     const seedDependencies = this.NPM_DEPENDENCIES;
 
